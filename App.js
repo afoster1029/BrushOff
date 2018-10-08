5e33e50997e0e548d81a061c72a7f5a3e3b5a58f
colinkirby <ckirby1@macalester.edu>
2018-10-07 22:30:53 -0500

    Added navigator to App.js

--- App.js
+++ App.js
@@ -1,96 +1,21 @@
 // In App.js in a new project
 import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-// import colin's stuff
-// import 'drawing.js'
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
+import HomeScreen from './screens/Home.js';
+import SettingsScreen from './screens/Settings.js';
+import DrawingScreen from './screens/Drawing.js';
 
+const Navigation = StackNavigator({
+    Home: {screen: HomeScreen},
+    Settings: {screen: SettingsScreen},
+    Drawing: {screen: DrawingScreen}
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home Screen',
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  };
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Settings"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Settings', {
-              itemId: 86,
-              otherParam: 'wubba lubba dub dub',
-            });
-          }}
-        />
-      </View>
-    );
-  }
-}
-
-class SettingsScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.getParam('otherParam', 'A Nested Settings Screen'),
-    };
-  };
-  render() {
-    /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherParam = navigation.getParam('otherParam', 'some default value');
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Settings Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to settings again..."
-          onPress={() =>
-            // example of how push can override the intial declaration of prop.
-            this.props.navigation.push('Settings', {
-              itemId: Math.floor(Math.random() * 100),
-            })}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-        <Button
-           title="Morty quote"
-           onPress={() => this.props.navigation.setParams({otherParam: 'Awww jeez ricky'})}
-         />
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Settings: SettingsScreen
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
+})
 
 export default class App extends React.Component {
-  render() {
-    return <RootStack />;
-  }
+    render() {
+        return <Navigation />;
+    }
 }

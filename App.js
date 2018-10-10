31571b64ebc3261ff81aaed83c92370c2fc8b490
Alexander Foster <alexanderpfoster@gmail.com>
2018-10-10 11:02:54 -0500

    changed StackNavigator to createStackNavigator. Also added buttons to home and settings page.

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 // In App.js in a new project
 import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
-import { StackNavigator, CreateStackNavigator } from 'react-navigation';
+import { StackNavigator, createStackNavigator } from 'react-navigation';
 
 import HomeScreen from './screens/Home.js';
 import SettingsScreen from './screens/Settings.js';
 import DrawingScreen from './screens/Drawing.js';
 
-const Navigation = StackNavigator({
+const Navigation = createStackNavigator({
     Home: {screen: HomeScreen},
     Settings: {screen: SettingsScreen},
     Drawing: {screen: DrawingScreen}

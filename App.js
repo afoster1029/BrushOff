c4042c13eb2e6138d8640814f9118df50509a998
Alexander Foster <alexanderpfoster@gmail.com>
2018-10-11 11:14:36 -0500

    worked on Lobby.js to make a function that would allow user to enter player names. Not complete yet.

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends React.Component {
 
 
 /*
-Found a link that shows how to make sounds. Could be a cool addition to when someone wins
+Found a link that shows how to make sounds. Could be a cool addition for when someone wins
 - https://hackernoon.com/how-i-built-a-super-simple-game-using-react-native-67bdade50373
 
 */

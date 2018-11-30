17160b57517c56b15cef80c657d993497276a042
samgnail <samgnail@gmail.com>
2018-11-30 11:50:44 -0600

    Temp Winner stuff

--- screens/Winner.js
+++ screens/Winner.js
@@ -14,17 +14,23 @@ export default class Voting extends React.Component {
     const { navigate } = this.props.navigation;
     const winnerUri = this.props.navigation.getParam('winningImage', 'no image');
     const winner = this.props.navigation.getParam('winnerName', 'nothing passed')
+    const playerInfo = this.props.navigation.getParam('playerInfo', 'nothing passed');
+    console.log('in Winner.js! '+ playerInfo);
+
     return (
       <View style = {styles.container}>
         <Text style= {{fontSize: 60, fontWeight: 'bold', textAlign: 'center'}}>Congrats {winner['name']}</Text>
-        <Image
-          style={{width: 200, height: 200}}
-          source={{uri: winnerUri}}
-        />
+        <View style = {styles.leaderboard}>
+          {playerInfo.map((player, idx)=> (
+            <View key = {idx}>
+              <Text> player.name + 'has ' + player.score + ' points!'</Text>
+            </View>
+          ))}
+        </View>
+
         <Button
           title="Next Round"
           onPress={() => {
-            /* 1. Navigate to the Details route with params */
             this.props.navigation.navigate('Categories', {
             });
           }}
@@ -32,11 +38,11 @@ export default class Voting extends React.Component {
         <Button
           title="Quit"
           onPress={() => {
-            /* 1. Navigate to the Details route with params */
             this.props.navigation.navigate('Home', {
             });
           }}
         />
+
       </View>
     )
   }
@@ -45,10 +51,19 @@ export default class Voting extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: windowWidth - 110,
+    height: windowHeight - 200,
+    borderRadius: 10,
+    marginBottom: 200,
+    marginTop: 200,
+    borderColor: 'grey',
+    borderWidth: 2,
+    opacity: .85,
+  },
+  leaderboard: {
+    flex: 1,
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    backgroundColor: 'transparent',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });

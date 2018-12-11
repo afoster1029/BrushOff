7e2bace7e1ddf4bb17f03bd8193ee1191d27271b
samgnail <samgnail@gmail.com>
2018-12-11 17:16:28 -0600

    Added Instructions to Settings screen.
    Added detailed game instructions to the settings screen for confused users.

--- screens/Settings.js
+++ screens/Settings.js
@@ -18,16 +18,17 @@ export default class Settings extends React.Component {
           <View style = {styles.instructionsBox}>
             <Text style= {{fontSize: 30, fontWeight: 'bold', textAlign: 'center', alignSelf: 'center'}}>Welcome to Brush Off!</Text>
             <Text style= {{fontSize: 20, fontWeight: 'bold', textAlign: 'center', alignSelf: 'center'}}>
-              Brush Off is a drawing competetion game! To start a round, click Start Game on the home screen
-              to go a lobby to start a new round! Enter your name, then pass the phone to another player
-              so they can enter their name.
+              Brush Off is a drawing competetion game! To start a round, click Start Game on the home screen;
+              you will be taken to a lobby to start a new round! Enter your name, then pass the phone to another player
+              so they can enter theirs. Here you can also edit the amount of time each player has to draw their piece.
 
-              When you click Start Game in the lobby, a player will be chosen to be the judge over this rounds drawings!
-              That judge will choose from four possible prompt categories that the other players will each take a turn drawing.
-              The artists are on a timer though, and will only have so much time to draw their masterpiece before
-              it is the next players turn and the previous player must pass their phone along.
+              When you click Start Game in the lobby, a player will be chosen as judge over this rounds drawings!
+              That judge will choose from four possible prompt categories that the other players will each take a
+              turn drawing. The judge will not draw on the drawing screen. The judge will change next round. The
+              artists are on a timer though, and will only have so much time to draw their masterpiece 
+              before it is the next players turn and the previous player must pass their phone along.
 
-              Once all players have had their turn, the judge will select the winning image, and that player
+              Once all players have had their turn the judge will select the winning image, and that player
               will receive one point for winning the round! From there you will be taken to a scoreboard screen
               where you can choose to go into a new round with the same players and points, or go back to the main menu.
             </Text>
@@ -59,13 +60,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: 'center',
     backgroundColor: 'white',
-    // width: windowWidth - 110,
-    // height: windowHeight - 200,
     borderRadius: 10,
-    marginBottom: 150,
-    marginTop: 150,
+    paddingBottom: 10,
+    marginTop: 15,
     borderColor: 'grey',
     borderWidth: 2,
     opacity: .85,
+    flexDirection: 'column',
+    alignItems: 'center',
   }
 });
